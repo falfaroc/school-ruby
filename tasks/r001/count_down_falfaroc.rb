b70ffe4471d53d2760e7f2b5601b5fa804142b9d
Fernando Enrique Alfaro Campos <falfaroc@uwo.ca>
2017-09-26 11:32:50 -0400

    RPT: r001 30
    Completed Step two from the list. Installing reek on windows took a bit longer
    because 'gem install reek' did not work so I had to go and download the
    missing dependancies. After I had reek installed, figuring out the different errors
    and looking up how to fix them took sometime.

--- tasks/r001/count_down_falfaroc.rb
+++ tasks/r001/count_down_falfaroc.rb
@@ -1,30 +1,36 @@
 #! /usr/bin/env ruby
 require 'minitest/autorun'
 
+# Minitest class for CountDown
 class CountDownTest < Minitest::Test
     def test_takes_120_seconds
         count_down = CountDown.new
-        # assert count_down.length_period(120) == "Equals"
         time = 120
-        assert_operator count_down.length_period(time), :==, time
+        assert_operator count_down.time_elapsed(time), :==, time
     end
 end
 
+# Handles the count in to 0 in seconds
 class CountDown
-    def length_period time
+    $start = 0
+    $finish = 0
+
+    def time_elapsed time
         print "Counting down till " + time.to_s + "\n"
-        wait_time = time
-        start = Time.now
+        sec_count_down(time)
+        diff = $finish - $start
+        diff.round
+    end
+
+    # :reek:DuplicateMethodCall
+    def sec_count_down wait_time
+        $start = Time.now
         wait_time.downto(1).each do |time_left|
             sleep 1
             print time_left.to_s + " seconds left\n" if (time_left % 60) == 0
         end
-        finish = Time.now
-        diff = finish - start
-        diff.round
-        # if time == diff.round
-        #     "Equals"
-        # end
+        $finish = Time.now
     end
+
 end
 
