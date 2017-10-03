03886073392a9f1ae9a8d04aa1e1bdd266292db3
Fernando Enrique Alfaro Campos <falfaroc@uwo.ca>
2017-10-02 22:09:06 -0400

    RPT: r002 30
    Modified the hash table to be a nested hash table so it handles the
    discounts differently. Created a Discount class for the specific
    discount per item.
    
    Looking into why my totals variable does not reset on object initialization.
    Next, modifying the totals calculation.

--- tasks/r002/checkout.rb
+++ tasks/r002/checkout.rb
@@ -1,18 +1,52 @@
+# handles discounts
+class Discount
+    def initialize(amount, value)
+        $amount = amount
+        $value = value
+    end
+
+    def get_amount
+        $amount
+    end
+
+    def get_value
+        $value
+    end
+
+    def calculate_discount
+        $amount * $value
+    end
+end
+
 #Hash map, first value is base price
 RULES = {
-    'A' => 50,
-    'B' => 30,
+    'A' => {
+            'base' => 50,
+            'discount' => Discount.new(3,130),
+            },
+    'B' => {
+            'base' => 30,
+            'discount' => Discount.new(2,45),
+            },
     'C' => 20,
     'D' => 15,
 }
 
 # Immitate a supermarket checkout system
 class CheckOut
-	def initialize(rules)
-		$C_RULES = rules
+    $total = 0
+    def initialize(rules)
+        # print rules['A']['base'].to_s + "\n"
+        $rules = rules
+        # print $rules['A']['discount'].get_amount.to_s
     end
     
     def total
-        print "TOTAL FUNCTION\n"
+         $total
+    end
+
+    def scan item
+        print "Scanning: " + item.to_s + "\n"
+        $total += $rules[item]['base']
     end
 end
