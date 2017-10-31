8d0d02429bfc9cef6f0b8d5ca5a3bdd5b31118d0
Fernando Campos <falfaroc@uwo.ca>
2017-10-31 17:14:16 -0400

    RPT: r006 15
    Completed Steps 2 and 3. The given example from step 2 covers all but 1 line
    so I added another test with an array of 6 elements so that it covers that
    final line.
    Moving to step 4 to solve all reek warnings.

--- tasks/r006/heap_sort_spec.rb
+++ tasks/r006/heap_sort_spec.rb
@@ -2,5 +2,17 @@ require 'spec_helper.rb'
 require './heap_sort.rb'
 
 describe HeapSort do
+  it "should initialize a new HeapSort" do
+    heap = HeapSort.new([2,3])
+  end
 
+  it "should return array ordered as [1, 2, 3]" do
+    heap = HeapSort.new([3, 2, 1])
+    expect(heap.heap_sort).to eq([1, 2, 3])
+  end
+
+  it "should return array ordered as [1, 3, 6, 7, 10, 15]" do
+    heap = HeapSort.new([6, 1, 15, 3, 10, 7])
+    expect(heap.heap_sort).to eq([1, 3, 6, 7, 10, 15])
+  end
 end
