ee3f781c8a1084eafa586feecf05dd33f5984a5e
Fernando Campos <falfaroc@uwo.ca>
2017-11-15 16:32:31 -0500

    RPT: r001 15
    Ensured reek was satisfied by all 3 files. In the test file, split up one of the functions because
    it was getting quite large of a function.

--- tasks/r001/count_down_test.rb
+++ tasks/r001/count_down_test.rb
@@ -11,31 +11,30 @@ class CountDownTest < Minitest::Test
 		CountDown.new(fake).count_down($time)
 		message = fake.log(1)
 		assert_equal message, $time.to_s + " seconds left"
-		# assert_operator CountDown.new(fake).count_down($time), :==, $time
-		# fake.timeInterval
 	end
 
+	# :reek:FeatureEnvy
 	def test_1_second_difference
 		fake = QuietPrintMethod.new
-		# fake = PrintMethod.new
 		CountDown.new(fake).count_down($time)
 
 		number_of_messages = fake.number_of_messages - 1
 		# print "NUM: " + number_of_messages.to_s
 		current_time = fake.time_log(number_of_messages)
 
-		if number_of_messages > 1 
-				(number_of_messages - 1).downto(1) { | next_message |
-						next_time = fake.time_log(next_message)
-						# print "C: " + current_time.to_s + + ", N: " + next_time.to_s + "\n"
-						# assert_operator next_time, :==, current_time + 1
-						assert (next_time + 1.1 > current_time && next_time + 1 < current_time)
-						current_time = next_time
-				}
-		end
+		calculate_time(fake, number_of_messages, current_time)
 	end
 
-	# def test_takes_120_seconds_print
-	# 	assert_operator CountDown.new(PrintMethod.new).count_down($time), :==, $time
-	# end
+	# :reek:FeatureEnvy
+	def calculate_time(fake, number_of_messages, current_time) 
+		if number_of_messages > 1 
+			(number_of_messages - 1).downto(1) { | next_message |
+					next_time = fake.time_log(next_message)
+					# print "C: " + current_time.to_s + + ", N: " + next_time.to_s + "\n"
+					# assert_operator next_time, :==, current_time + 1
+					assert (next_time + 1.1 > current_time && next_time + 1 < current_time)
+					current_time = next_time
+			}
+		end
+	end
 end
