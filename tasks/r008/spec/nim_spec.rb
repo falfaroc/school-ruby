ea7d2cb99dd3238fd6fca845987221aff87acb5d
Fernando Campos <falfaroc@uwo.ca>
2017-11-21 21:49:35 -0500

    RPT: r008 20
    Working on step 4. Improved the simplecov coverage to 79.74. In order to play an entire game,
    I brute forced the inputs for the user. Need to see if there is any other way to go about
    that.
    
    Not any issues arose other than variable input depending on output has not been looked at.

--- tasks/r008/spec/nim_spec.rb
+++ tasks/r008/spec/nim_spec.rb
@@ -6,12 +6,8 @@ describe Nim do
     before (:each) do
         #Initialize object
         @Nim = Nim.new(Player.new)
-        # @output = StringIO.new
 
-        # @input = StringIO.new("test info\n")
-        # @output = StringIO.new
-        # @input_received = @ui.receive(@input)
-        # @ui.give("test message", @output)
+        $stdout.stub(:write) #supressing stdout
     end
 
     #after smart computer makes the move, check_kernel_state should return true
@@ -63,10 +59,7 @@ describe Nim do
     end
 
     it 'start game, config board' do
-        io = StringIO.new
-        io.puts '1'
-        io.puts '1'
-        io.rewind
+        io = StringIO.new("1\n1\n")
 
         $stdin = io
 
@@ -75,33 +68,37 @@ describe Nim do
     end
 
     it 'start game, config board, run game' do
-        expectation = expect { puts "Welcome to NIM!"; 
-                               puts "1: [1, 3, 5, 7]";}
-        expectation.to output("Welcome to NIM!\n1: [1, 3, 5, 7]\n").to_stdout
+        test_inp = "1\n
+                    1\n
+                    1\n1\n
+                    2\n1\n2\n2\n
+                    3\n1\n3\n2\n3\n3\n
+                    4\n1\n4\n2\n4\n3\n4\n4\n
+                    5\n1\n5\n2\n5\n3\n5\n4\n5\n5\n"
 
-        # @Nim.start_game
-        # io = StringIO.new
-        # io.puts '1'
-        # io.puts '1'
-        # io.rewind
+        io = StringIO.new(test_inp)
+        
+        $stdin = io
 
-        # $stdin = io
+        # Kind of working with this
+        # expectation = expect { @Nim.start_game }
+        # expectation.to output("Welcome to NIM!\n1: [1, 3, 5, 7]\n").to_stdout
 
-        # @Nim.start_game
-        # @Nim.configBoard
+        @Nim.start_game
+        @Nim.configBoard
 
-        # while @Nim.gameOver == false
-        #     @Nim.display
-        #     @Nim.humanMakeMove
-        #     @Nim.checkGameOver
-        #     if @Nim.gameOver == false
-        #       @Nim.computerMakeMove
-        #       if @Nim.gameOver == true
-        #         puts "Computer player wins!"
-        #       end
-        #     else
-        #       puts "Human player wins!"
-        #     end
-        #   end
+        while @Nim.gameOver == false
+            @Nim.display
+            @Nim.humanMakeMove
+            @Nim.checkGameOver
+            if @Nim.gameOver == false
+              @Nim.computerMakeMove
+              if @Nim.gameOver == true
+                puts "Computer player wins!"
+              end
+            else
+              puts "Human player wins!"
+            end
+          end
     end
 end
